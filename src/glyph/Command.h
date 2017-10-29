96f8fedccc81336fa9292689c5665afd05c756f9
kitworker <kasyanov@gmail.com>
2017-10-29 14:54:14 +0300

    need log for test

--- src/glyph/Command.h
+++ src/glyph/Command.h
@@ -16,29 +16,50 @@ public:
 	virtual ~ICommand();
 	virtual void Execute(TContext) = 0;
 	virtual void Unexecute(TContext) = 0;
+	virtual char* PrintLog(const char*);
 
 protected:
 	ICommand();
 };
 
 template <class TContext>
-class CommandTimer {
+class CommandTimer : public ICommand<TContext> {
+public:
+	virtual ~CommandTimer();
 
+	CommandTimer();
+	virtual void Execute(TContext);
+	virtual void Unexecute(TContext);
+};
+
+
+template <class TContext>
+class CommandUser: public ICommand<TContext> {
+public:
+	virtual ~CommandUser();
+	CommandUser();
+	virtual void Execute(TContext);
+	virtual void Unexecute(TContext);
 };
 
 /* Use this class object as a branch messages parameter
  * Object command as a strategy
+ *
+ *
+ * Execute/Unexecute предосталяет доступ ко всем сомандам
+ * но будут ли они выполнены решает контекст(proxy)
+ *
  */
 template <class TContext>
 class MacroCommand : ICommand<TContext> {
 public: MacroCommand();
 	virtual ~MacroCommand();
-	virtual void Execute(TContext) ;
+	// TODO Add()... Remove()...
+	virtual void Execute(TContext proxy) ;
 	virtual void Unexecute(TContext);
 private:
 	// The collection contains the history of the commands any client
 	Collection<ICommand<TContext>, std::list> mCmd;
-
 };
 
 
