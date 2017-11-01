0a069c8d59d29e33ea6dff7e9f80c7a0913d7251
kitworker <kasyanov.nn@gmail.com>
2017-11-01 16:04:28 +0200

    before pull

--- main.cpp
+++ main.cpp
@@ -6,15 +6,22 @@
 
 #include "src/glyph/Glyph.h"
 
+#include "src/glyph/Proto.h"
+
+#include "src/Collection.h"
+#include <vector>
 
 using namespace std;
 
 
 int main() {
 	
+
+	Collection<int, vector  > container;
 	std::string t;
 	int id = 1;
 	Glyph* glyph = new Frame(id);
+
 
 	return 0;
 }
